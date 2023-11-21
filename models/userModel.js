933f18d746db5030740f509dd23d216b6f9dda6c
ErikLabuda <labe01@vse.cz>
2023-11-21 15:49:02 +0000

    Merge branch 'main' of https://gitlab.com/FIS-VSE/4IT115/2023ZS/st1430/labe01/financyplan

--- models/userModel.js
+++ models/userModel.js
@@ -1,12 +1,112 @@
 const mongoose = require('mongoose')
-
+const Schema = mongoose.Schema;
 // Definice schématu pro model uživatele
-const UserSchema = new mongoose.Schema({
-    name: { type: String, required: true }, // Jméno uživatele, povinné a unikátní
-    surname: { type: String, required: true }, // Příjmení uživatele, povinné
-    email: { type: String, required: true, unique: true }, // E-mail uživatele, povinný a unikátní
-    password: { type: String, required: true } // Heslo uživatele, povinné
-    
+const UserSchema = new mongoose.Schema(
+    {
+  name: {
+    type: String,
+    required: true
+  },
+  surname: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  bankAccounts: {
+    _bankId: {
+      type: Schema.Types.ObjectId
+    },
+    bankInfo: {
+      type: String
+    },
+    bankNumber: {
+      type: String
+    }
+  },
+  brokerAccounts: {
+    _brokerId: {
+      type: Schema.Types.ObjectId
+    },
+    brokerInfo: {
+      type: String
+    },
+    brokerLogin: {
+      type: String
+    },
+    brokerPassword: {
+      type: String
+    }
+  },
+  incomes: {
+    _incomeId: {
+      type: Schema.Types.ObjectId
+    },
+    category: {
+      type: String
+    },
+    amount: {
+      type: Number
+    },
+    date: {
+      type: Date
+    },
+    regular: {
+      type: Boolean
+    }
+  },
+  expenses: {
+    _expenseId: {
+      type: Schema.Types.ObjectId
+    },
+    category: {
+      type: String
+    },
+    amount: {
+      type: Number
+    },
+    date: {
+      type: Date
+    },
+    regular: {
+      type: Boolean
+    }
+  },
+  investments: {
+    _investmentId: {
+      type: Schema.Types.ObjectId
+    },
+    ticker: {
+      type: String
+    },
+    type: {
+      type: String
+    },
+    amount: {
+      type: Number
+    },
+    date: {
+      type: Date
+    },
+    regular: {
+      type: Boolean
+    },
+    isOpen: {
+      type: Boolean
+    },
+    openingPrice: {
+      type: Number
+    },
+    closingPrice: {
+      type: Number
+    }
+  }
 }, 
 { collection: 'users' } // Název kolekce (tabulky) v MongoDB, kam budou ukládána data
 )
